7b8eec3485b8e55db3d46907b04efdab77c701c9
sheetal tiwari <sheetaltiwari255@gmail.com>
2024-11-20 16:26:49 +0530

    banner center banner bottom

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,8 @@ const  categoryController  = require('../controllers/categoryController');
 const  dealsController  = require('../controllers/dealsController');
 const  brandsController  = require('../controllers/brandsController');
 const  bannerController  = require('../controllers/bannerController');
+const BannerCenterController = require('../controllers/centerBannerController');
+const BannerBottomController = require('../controllers/bottomBannerController');
 const packagesController = require('../controllers/packagesController');
 const { authMiddleware } = require('../middleware/auth');
 const router = express.Router();
@@ -24,4 +26,8 @@ router.get('/getall-deals',authMiddleware, dealsController.getAllDeals);
 router.get('/package',authMiddleware, packagesController.getallactivePackages);    
 // Get all banners with pagination and search
 router.get('/get-all-banners', authMiddleware, bannerController.getAllBanners);
+// Get all banners with pagination and search
+router.get('/get-allcenter-banners', authMiddleware, BannerCenterController.getAllBanners);
+// Get all banners with pagination and search
+router.get('/get-allbottom-banners', authMiddleware, BannerBottomController.getAllBanners);
 module.exports = router;
