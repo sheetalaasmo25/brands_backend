3f66ccdec867f41deb63e988dcc91314207e55b7
sheetal tiwari <sheetaltiwari255@gmail.com>
2024-11-28 11:26:14 +0530

    cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const brandRoutes = require('./routes/brandRoutes')
 const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-  origin: ['http://localhost:3000'], 
+  origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true, 
